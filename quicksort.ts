ca4233ca8e96ee231203aac2da5129c7e4e96b24
wireless4024 <wireless4024@proton.me>
2022-10-10 20:43:51 +0700

    [1.1] Swapping element
    In this step we will implement a swap function to swap element in an array

--- quicksort.ts
+++ quicksort.ts
@@ -1,3 +1,9 @@
 function quicksort(arr: number[]) {
 	// TODO: Implement quicksort here
 }
+
+function swap(arr: number[], i: number, j: number) {
+	let temp = arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
+}
