a9312068f04adfb482bbfdd91a9a7e5302c50ea7
wireless4024 <wireless4024@proton.me>
2022-10-10 20:45:54 +0700

    [1.2] Partitioning
    This function takes last element as pivot, places the pivot element at its correct position in sorted array, and places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot.

--- quicksort.ts
+++ quicksort.ts
@@ -7,3 +7,15 @@ function swap(arr: number[], i: number, j: number) {
 	arr[i] = arr[j]
 	arr[j] = temp
 }
+
+function partition(arr: number[], low: number, high: number) {
+	let pivot = arr[high]
+	let i = (low - 1)
+	for (let j = low; j <= high - 1; j++) {
+		if (arr[j] < pivot) {
+			swap(arr, ++i, j)
+		}
+	}
+	swap(arr, i + 1, high)
+	return (i + 1)
+}
